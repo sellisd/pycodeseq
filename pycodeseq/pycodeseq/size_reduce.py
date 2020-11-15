76d0cb8667a0a1705fb6d3d9928f8c43e56894ea
Diamantis Sellis <sellisd@gmail.com>
2020-11-15 13:30:09 +0100

    Correct dry run option to be boolean

--- pycodeseq/pycodeseq/size_reduce.py
+++ pycodeseq/pycodeseq/size_reduce.py
@@ -6,15 +6,17 @@ import click
 @click.option('--root_path', default='/mnt/Data/scratch',
               show_default=True,
               help='Delete files in all subdirectories')
-@click.option('--extensions', default=['py', 'ipynb', 'java'],
+@click.option('--extensions', default=['.py', '.ipynb', '.java'],
               help='Exclude files with given extension',
               show_default=True,
               multiple=True)
-@click.option('--dry-run', default=True, show_default=True,
+@click.option('--dry-run/--no-dry-run', default=True, show_default=True,
               help='List files to be deleted')
 def clean(root_path, extensions, dry_run):
     for file in Path(root_path).glob(f"**/*"):
-        if (file.is_file()) and (file.suffix not in extensions):
-            print(file)
-            if not dry_run:
-                file.unlink()
+        if file.is_file():
+            if file.suffix not in extensions:
+                print(file)
+                if (not dry_run):
+                    file.unlink()
+
