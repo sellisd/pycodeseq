2a944c64110a2588b100bda9924fc9f9c241c101
Diamantis Sellis <sellisd@gmail.com>
2020-11-28 12:45:58 +0100

    Added test for count command and its options

--- pycodeseq/test_pycodeseq.py
+++ pycodeseq/test_pycodeseq.py
@@ -5,6 +5,7 @@ import json
 from collections import Counter
 
 import pytest
+from click.testing import CliRunner
 
 import pycodeseq
 
@@ -29,7 +30,7 @@ def test_count_levels(tmp_path):
 
 
 def test_parse_notebooks(tmp_path):
-    input_file = './test_data/notebook_example.py'
+    input_file = './test_data/notebook_example.ipynb'
     output_file = tmp_path / 'cells_output.tsv'
     template_output_file = './test_data/notebook_example.tsv'
     with open(output_file, 'w') as f:
@@ -38,6 +39,15 @@ def test_parse_notebooks(tmp_path):
     assert(filecmp.cmp(output_file, template_output_file, shallow=False))
 
 
-
-def test_count():
-    pass
+@pytest.mark.parametrize("method, template_output_file",
+                         [("levels", "./test_data/test_count_levels.tsv"),
+                          ("cells", "./test_data/test_count_cells.tsv"),
+                          ("tokens", "./test_data/test_count_tokens.tsv")])
+def test_count(tmp_path, method, template_output_file):
+    runner = CliRunner()
+    input_path = './test_data/'
+    output_file = tmp_path / 'data.tsv'
+    ressults = runner.invoke(pycodeseq.count, ['--input_path', input_path,
+                                               '--output', output_file,
+                                               '--method', method])
+    assert(filecmp.cmp(output_file, template_output_file, shallow=False))
